107991e3df8b4630b86b53459150f4d762dfa962
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-08-09 23:30:40 -0700

    validation

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    //
+    // Login Method
+
+    public function login(){
+
+    }
+
+    // Registration Method
+
+    public function register(RegisterRequest $request)
+    {
+
+    }
 }
